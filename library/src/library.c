cf367b0e8021373a99a1201f6a6ecb7088f578a0
elecro <galpeter@inf.u-szeged.hu>
2016-04-11 13:57:54 +0200

    Minor style fix (#1)
    Move function opening '{'-s to a new line.

--- library/src/library.c
+++ library/src/library.c
@@ -4,16 +4,19 @@
 
 #include "library.h"
 
-int hello(char* text) {
+int hello(char* text)
+{
     return printf("LIB: %s\n", text);
 }
 
-float distance(float x1, float y1, float x2, float y2) {
+float distance(float x1, float y1, float x2, float y2)
+{
     float x_diff = (x1 - x2);
     float y_diff = (y1 - y2);
     return sqrt(x_diff * x_diff + y_diff * y_diff);
 }
 
-void print_zlib() {
+void print_zlib()
+{
     printf("LIB: %s\n", zlibVersion());
 }
--- program/src/main.c
+++ program/src/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "library.h"
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
     hello("Demo");
 
     printf("distance ((1,2), (4,4)) = %.f\n", distance(1, 2, 4, 4));
